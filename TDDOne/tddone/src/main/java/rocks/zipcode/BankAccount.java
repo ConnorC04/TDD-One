086ab7a537eeb13da270ed013f46a01973b8a2cc
ConnorC04 <connorjcasey@iCloud.com>
2025-03-07 09:35:00 -0500

    bank account tests passing, calc will be done soon

--- TDDOne/tddone/src/main/java/rocks/zipcode/BankAccount.java
+++ TDDOne/tddone/src/main/java/rocks/zipcode/BankAccount.java
@@ -11,31 +11,53 @@ package rocks.zipcode;
 // Student must implement this class
 public class BankAccount {
     // TODO: Implement this class to make the tests pass
-    
+
+    private String accNum;
+    private String ownName;
+    private double accBalance = 0.0;
+
     public BankAccount(String accountNumber, String ownerName) {
         // Your implementation here
+        this.accNum = accountNumber;
+        this.ownName = ownerName;
+        this.accBalance = accBalance;
     }
     
     public String getAccountNumber() {
         // Your implementation here
-        return null;
+        return this.accNum;
     }
     
     public String getOwnerName() {
         // Your implementation here
-        return null;
+        return this.ownName;
     }
     
     public double getBalance() {
         // Your implementation here
-        return 0.0;
+        return this.accBalance;
     }
     
     public void deposit(double amount) {
-        // Your implementation here
+        if (amount < 0){
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+        else{
+            this.accBalance += amount;
+        }
     }
     
     public void withdraw(double amount) {
         // Your implementation here
+        if (this.accBalance < amount){
+            throw new IllegalStateException("Insufficient funds");
+        }else{
+            if(amount < 0){
+                throw new IllegalArgumentException("Withdrawal amount must be positive");
+            }else{
+                this.accBalance -= amount;
+            }
+
+        }
     }
 }
