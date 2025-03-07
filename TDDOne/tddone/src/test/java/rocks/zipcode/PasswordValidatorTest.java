89d3e28133e0e546dd04edf3cfaa9c7af06069a2
ConnorC04 <connorjcasey@iCloud.com>
2025-03-07 13:13:16 -0500

    done PasswordValidator

--- TDDOne/tddone/src/test/java/rocks/zipcode/PasswordValidatorTest.java
+++ TDDOne/tddone/src/test/java/rocks/zipcode/PasswordValidatorTest.java
@@ -1,13 +1,38 @@
 package rocks.zipcode;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 
 // Student should write this test class
 public class PasswordValidatorTest {
+
+    private PasswordValidator validator;
+
+    public void setUp(){
+        this.validator = new PasswordValidator();
+    }
     // TODO: Write tests for the PasswordValidator class
     // Test cases should include:
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 20;
     // - Valid passwords
+    @Test
+    public void isValidPassword(){
+        setUp();
+        assertTrue(validator.isValid("ValidPassword-1"));
+        assertTrue(validator.isValid("Another3xample!"));
+        assertTrue(validator.isValid("0neMore4-theRoad"));
+        assertFalse(validator.isValid("short"));
+        assertFalse(validator.isValid(null));
+        assertFalse(validator.isValid(("12345")));
+        assertFalse(validator.isValid("ALLUPPERCASE"));
+        assertFalse(validator.isValid("alllowercase"));
+        assertFalse(validator.isValid("thispasswordiswaytoolongandwontworkinthisapplicationsotryagainkris"));
+        assertFalse(validator.isValid(""));
+        assertFalse(validator.isValid("!!!???---+++==="));
+    }
     // - Passwords that are too short or too long
     // - Passwords missing uppercase, lowercase, digits, or special chars
     // - Edge cases like null or empty strings
